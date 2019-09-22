08378544a4bba27cf8845995798d04619d0d59bd
Elliot Briant <elliotbriant@Elliots-MacBook-Air.local>
2019-09-22 12:07:51 -0700

    Adding comments

--- lanes.py
+++ lanes.py
@@ -3,7 +3,15 @@ import numpy as np
 
 image = cv2.imread('./Image/test_image.jpg')
 lane_image = np.copy(image)
+
 # Converting the image to gray
 gray = cv2.cvtColor(lane_image, cv2.COLOR_BGR2GRAY)
+
+# Converting image with the rbg
+# cv2.imshow('result', image)
+
+# Display image until you press any key of the keyboard 
 cv2.imshow('result', gray)
 cv2.waitKey(0)
+
+
