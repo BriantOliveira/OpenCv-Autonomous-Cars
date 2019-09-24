a6f7fcba977ff87adfb3f7e2780b0ce0e22bc8cb
Elliot Briant <elliotbriant@Elliots-MacBook-Air.local>
2019-09-23 17:13:30 -0700

    Ading line frame

--- traffic_lanes.py
+++ traffic_lanes.py
@@ -10,11 +10,20 @@ def canny(image):
     return canny
 
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
+
+
 def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([[200, height], [1100, height], [550, 250]])
     mask = np.zeros_like(image)
-    cv2.fillPoly(mask, [polygons],   255)
+    cv2.fillPoly(mask, [polygons], 255)
     masked_image = cv2.bitwise_and(image, mask)
     return masked_image
 
@@ -23,6 +32,8 @@ image = cv2.imread('./Image/test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
 
-cv2.imshow("result", region_of_interest(cropped_image))
+cv2.imshow("result", line_image)
 cv2.waitKey(0)
